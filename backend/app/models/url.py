0335174c36895281e710445ad91ad979e8c3295a
adewale-codes <adewaleabiola78@gmail.com>
2024-02-29 15:57:48 +0100

    linked project to database and created models for the tables in database

---
+++ backend/app/models/url.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.db.database import Base
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    original_url = Column(String, index=True)
+    short_url = Column(String, index=True, unique=True)
+    custom_alias = Column(String, index=True, nullable=True)
+    click_count = Column(Integer, default=0) 
+    qr_code_path = Column(String, nullable=True) 
