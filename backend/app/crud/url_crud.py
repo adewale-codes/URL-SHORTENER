ab8941086d1e0059e9f9ec7065e6ff49f0c8e6d2
adewale-codes <adewaleabiola78@gmail.com>
2024-02-29 16:30:44 +0100

    addded the qr code part to the create url function to both generate and save the qr code

--- backend/app/crud/url_crud.py
+++ backend/app/crud/url_crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.url import URL
 import shortuuid
 from fastapi import HTTPException
+from app.utils.qr_code_generator import save_qr_code
 
 def create_url(db: Session, original_url: str, custom_alias: str = None):
     short_url = shortuuid.uuid()[:8]
@@ -16,6 +17,9 @@ def create_url(db: Session, original_url: str, custom_alias: str = None):
     db.add(url_db)
     db.commit()
     db.refresh(url_db)
+
+    qr_code_path = save_qr_code(short_url)
+    url_db.qr_code_path = qr_code_path
 
     db.commit()
 
---
+++ backend/app/utils/qr_code_generator.py
@@ -0,0 +1,23 @@
+import qrcode
+import os
+
+QR_CODE_DIR = "qr_codes"  
+
+def save_qr_code(short_url: str) -> str:
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(short_url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    os.makedirs(QR_CODE_DIR, exist_ok=True)
+
+    img_path = os.path.join(QR_CODE_DIR, f"{short_url}.png")
+    img.save(img_path)
+    
+    return img_path
